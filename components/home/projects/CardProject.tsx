3ad1d8924484ae6b405ce39bc422b0e8661905c5
matias <matifandy@gmail.com>
2022-06-14 19:51:47 -0300

    cardproject ui

--- components/home/projects/CardProject.tsx
+++ components/home/projects/CardProject.tsx
@@ -8,18 +8,18 @@ import styles from "../../../styles/CardProject.module.scss";
 const CardProject = () => {
   return (
     <div className={styles.CardProject}>
-      <h1></h1>
+      <h1>My App</h1>
       <Image
         alt="alt"
         src={pic}
         width={100}
         height={100}
       />
-      <div className="links_tags">
+      <div className={styles.CardProject__tags}>
         <a href="">Source</a>
         <a href="">Demo</a>
       </div>
-      <p>description</p>
+      <p>Description</p>
     </div>
   );
 };
